721eca75bcdbdbbfa6e9127798e70f7628bed84e
Dinesh kumar <72866547+Deathslayer89@users.noreply.github.com>
2023-05-29 22:58:31 +0530

    Update deletelatest.js

--- src/pages/api/mongo/deletelatest.js
+++ src/pages/api/mongo/deletelatest.js
@@ -2,7 +2,6 @@
  * @param {import('next').NextApiRequest} req
  * @param {import('next').NextApiResponse} res
  */
-import Attendance from '@/models/Attendance';
 import connectMongo from '../../../lib/mongodb';
 import Attendance from '@/models/Attendance';
 export default async function handler(req, res) {
